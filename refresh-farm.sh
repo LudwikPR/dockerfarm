1f7ea0a5d5d38ad4c24bf567b199b900d4298683
LudwikPR <25858137+LudwikPR@users.noreply.github.com>
2017-10-25 21:25:44 -0400

    Useless sudo?

--- refresh-farm.sh
+++ refresh-farm.sh
@@ -1,9 +1,9 @@
-sudo apt-get purge -y docker.io
-sudo apt-get autoremove -y --purge docker.io
-sudo apt-get autoclean -y
-sudo rm -rf /etc/docker
-sudo rm -rf /var/lib/docker
-sudo rm /etc/apparmor.d/docker
-sudo groupdel docker
-sudo apt install docker.io -y
-sudo docker run -d --restart=unless-stopped -p 8080:8080 rancher/server
+apt-get purge -y docker.io
+apt-get autoremove -y --purge docker.io
+apt-get autoclean -y
+rm -rf /etc/docker
+rm -rf /var/lib/docker
+rm /etc/apparmor.d/docker
+groupdel docker
+apt install docker.io -y
+docker run -d --restart=unless-stopped -p 8080:8080 rancher/server
